c8ee322501f663dbdd79d058654295d6a85fb958
Reymon99 <s.maje@udla.edu.co>
2020-03-20 22:18:55 -0500

    Patron Singleton a la clase Desktop.java

--- src/gui/principal/Desktop.java
+++ src/gui/principal/Desktop.java
@@ -2,12 +2,17 @@ package gui.principal;
 import tools.Archivos;
 import java.awt.*;
 import java.util.Objects;
+import java.util.Optional;
 import javax.swing.*;
 public final class Desktop extends JFrame {
     /**
+     * Apuntador de la instancia de Desktop
+     */
+    private static Desktop instance;
+    /**
      * Frame del proyecto
      */
-    public Desktop() {
+    private Desktop() {
         super("Seros");
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         setLayout(new GridBagLayout());
@@ -16,5 +21,14 @@ public final class Desktop extends JFrame {
         setIconImage(Objects.requireNonNull(
                 Archivos.image("/resources/image/icon.png", -1, -1)).getImage()
         );
+    }
+
+    /**
+     * Patron Singleton de Desktop
+     * @return Instancia del Frame
+     */
+    public static Desktop getInstance() {
+        if (Optional.ofNullable(instance).isEmpty()) instance = new Desktop();
+        return instance;
     }
 }
--- src/main/Main.java
+++ src/main/Main.java
@@ -2,6 +2,6 @@ package main;
 import gui.principal.Desktop;
 public class Main { //5337 lines
     public static void main(String[] args) {
-        new Desktop().setVisible(true);
+        Desktop.getInstance().setVisible(true);
     }
 }
