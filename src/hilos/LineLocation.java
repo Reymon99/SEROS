b17a187ee01b61fc311ea140545548bc55ef980c
Reymon99 <s.maje@udla.edu.co>
2020-04-10 03:35:28 -0500

    [MODIFIED] Implementación de Java Record en LineLocation * Se modificó la clase LineLocation * Se modificaron las llamadas de LineLocation en otras clases * Se pasó de lambda a referencia de método en la ejecución de las acciones de LineLocation

--- src/hilos/LineLocation.java
+++ src/hilos/LineLocation.java
@@ -1,89 +1,45 @@
 package hilos;
-public class LineLocation {
-    private int code;
-    private int line;
-    private String texto;
-    private int scroll;
-    private boolean sleep;
+
+public record LineLocation(
+        int code,
+        int line,
+        String texto,
+        int scroll,
+        boolean sleep
+) {
     /**
      * Locación de cada código en los editores con su respectivo texto a utilizar
-     * @param code ubicación de código a emplear
-     * @param line línea en el código a seleccionar
-     * @param texto texto respectivo de la línea
+     * @param code   ubicación de código a emplear
+     * @param line   línea en el código a seleccionar
+     * @param texto  texto respectivo de la línea
      * @param scroll posición del scroll del editor
      */
     public LineLocation(int code, int line, String texto, int scroll) {
         this(code, line, texto, scroll, true);
     }
+
     /**
      * Locación de cada código en los editores con su respectivo texto a utilizar
-     * @param code ubicación de código a emplear
-     * @param line línea en el código a seleccionar
+     * @param code  ubicación de código a emplear
+     * @param line  línea en el código a seleccionar
      * @param texto texto respectivo de la línea
-     * @param sleep si es verdadero duerme la ejecución de la línea de lo contrario solo se seleccionará la línea y el proceso seguirá normalmente
+     * @param sleep si es verdadero duerme la ejecución de la línea de lo contrario solo se seleccionará la línea y
+     *              el proceso seguirá normalmente
      */
     public LineLocation(int code, int line, String texto, boolean sleep) {
         this(code, line, texto, 0, sleep);
     }
+
     /**
      * Locación de cada código en los editores con su respectivo texto a utilizar
-     * @param code ubicación de código a emplear
-     * @param line línea en el código a seleccionar
+     * @param code  ubicación de código a emplear
+     * @param line  línea en el código a seleccionar
      * @param texto texto respectivo de la línea
      */
     public LineLocation(int code, int line, String texto) {
         this(code, line, texto, 0, true);
     }
-    /**
-     * Locación de cada código en los editores con su respectivo texto a utilizar
-     * @param code ubicación de código a emplear
-     * @param line línea en el código a seleccionar
-     * @param texto texto respectivo de la línea
-     * @param scroll posición del scroll del editor
-     * @param sleep si es verdadero duerme la ejecución de la línea de lo contrario solo se seleccionará la línea y el proceso seguirá normalmente
-     */
-    private LineLocation(int code, int line, String texto, int scroll, boolean sleep) {
-        this.code = code;
-        this.line = line;
-        this.texto = texto;
-        this.scroll = scroll;
-        this.sleep = sleep;
-    }
-    /**
-     * Obtiene la ubicación del código a emplear
-     * @return ubicación del código
-     */
-    public int getCode() {
-        return code;
-    }
-    /**
-     * Obtiene la línea a seleccionar en el código empleado
-     * @return línea a seleccionar del código en el Editor
-     */
-    public int getLine() {
-        return line;
-    }
-    /**
-     * Obtiene el texto empleado de la línea a seleccionar del código en el editor
-     * @return texto empleado de la línea
-     */
-    public String getTexto() {
-        return texto;
-    }
-    /**
-     * Obtiene la posición dada al scroll del editor
-     * @return posición del scroll
-     */
-    public int getScroll() {
-        return scroll;
-    }
-    /**
-     * Obtiene el estado de sí la línea duerme o no
-     * @return estado de espera
-     */
-    public boolean isSleep() {
-        return sleep;
-    }
+
     @Override
     public String toString() {
         return texto;
--- src/hilos/Lines.java
+++ src/hilos/Lines.java
@@ -1,41 +1,57 @@
 package hilos;
+
 import eventos.Eventos;
 import gui.editor.Editor;
 import gui.simulador.Simulador;
+
 import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
+
 public abstract class Lines extends Thread {
-    private Simulador simulador;
-    private List<LineLocation> lines;
+    private final Simulador simulador;
+    private final List<LineLocation> lines;
+
     /**
      * Hilo recorredor de líneas en el Editor.<br>
      * Modifica el texto del componente en determinadas líneas.
      * Habilita el boton deseado a utilizar
-     * @param simulador simulador correspondiente en el cual el componente {@link gui.contenido.Components#getBoton} se ubicarán los textos de las líneas correspondientes, y el componente {@link Editor} en el cual se seleccionarán las líneas dispuestas en {@link Lines#lines}.
-     * @param lines números de las líneas a seleccionar con sus respectivos texto, desde nulos a su contenido correspondiente.
+     * @param simulador simulador correspondiente en el cual el componente {@link gui.contenido.Components#getBoton}
+     *                  se ubicarán los textos de las líneas correspondientes, y el componente {@link Editor} en el
+     *                  cual se seleccionarán las líneas dispuestas en {@link Lines#lines}.
+     * @param lines     números de las líneas a seleccionar con sus respectivos texto, desde nulos a su contenido
+     *                  correspondiente.
      */
     protected Lines(Simulador simulador, LineLocation... lines) {
         this.simulador = simulador;
         this.lines = Arrays.asList(lines);
     }
+
     @Override
     public void run() {
         lines();
         actions();
     }
+
     /**
      * Selección de líneas en la ejecución del paso a paso
      */
-    private void lines(){
-        lines.forEach(e -> {
-            simulador.getCodigos().setSelectedIndex(e.getCode());
-            Eventos.scroll(((Editor) simulador.getCodigos().getComponentAt(e.getCode())), e.getScroll());
-            if (e.getLine() > 0) ((Editor) simulador.getCodigos().getComponentAt(e.getCode())).drawLineIn(e.getLine());
-            if (Optional.ofNullable(e.getTexto()).isPresent()) simulador.setTexto(e.getTexto());
-            if (e.isSleep()) Eventos.sleep(1270);
-        });
+    private void lines() {
+        lines.forEach(this::lineAction);
     }
+
+    /**
+     * Ejecuta cada una de las acciones de la línea empleada
+     * @param line línea del Editor a emplear
+     */
+    private void lineAction(LineLocation line) {
+        simulador.getCodigos().setSelectedIndex(line.code());
+        Eventos.scroll(((Editor) simulador.getCodigos().getComponentAt(line.code())), line.scroll());
+        if (line.line() > 0) ((Editor) simulador.getCodigos().getComponentAt(line.code())).drawLineIn(line.line());
+        if (Optional.ofNullable(line.texto()).isPresent()) simulador.setTexto(line.texto());
+        if (line.sleep()) Eventos.sleep(1270);
+    }
+
     /**
      * Acciones a realizar al final del hilo.<br>
      * Reescribirlo para efectuar los comandos.
